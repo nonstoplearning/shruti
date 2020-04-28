93f80a0df1963bcf8d98df548b87527ade7aa525
nonstoplearning <non.learning@gmail.com>
2020-04-28 20:22:17 +0530

    lol

---
+++ ex8.py
@@ -0,0 +1,11 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(
+"Twinkle Twinkle",
+"Little star",
+"How I wonder",
+"What you are"
+))
